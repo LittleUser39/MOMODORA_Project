a822d229748da4bb1f02dad39b67c4e0fbffe8a3
LittleUser39 <anjunh981@gmail.com>
2022-03-29 00:35:06 +0900

    집에서 수정

--- WinApi2D/CBossWarp.cpp
+++ WinApi2D/CBossWarp.cpp
@@ -3,13 +3,21 @@
 #include "CTexture.h"
 #include "CCollider.h"
 #include "CScene.h"
+#include "CAnimator.h"
 
 CBossWarp::CBossWarp()
 {
-	SetScale(fPoint(50, 80));
-	CD2DImage* pImg = CResourceManager::getInst()->LoadD2DImage(L"BossWarp", L"texture\\BossWarp.png");
+	SetScale(fPoint(100, 100));
+	
 	CreateCollider();
 	GetCollider()->SetScale(fPoint(50, 80));
+
+	CD2DImage* pImg = CResourceManager::getInst()->LoadD2DImage(L"BossWarp", L"texture\\BossWarp.png");
+	
+	CreateAnimator();
+	
+	GetAnimator()->CreateAnimation(L"BossWarp1", pImg, fPoint(0.f, 0.f), fPoint(96.f, 96.f), fPoint(96, 0), 1, 0.1f, true);
+	GetAnimator()->Play(L"Bosswarp1");
 }
 
 CBossWarp::~CBossWarp()
@@ -29,6 +37,7 @@ void CBossWarp::update()
 void CBossWarp::render()
 {
 	component_render();
+	GetAnimator()->update();
 }
 
 void CBossWarp::OnCollisionEnter(CCollider* other)
